3376796a4fd201300e4aa01adccd53ab5054b63d
Ana Helena <anahelenarp@hotmail.com>
2025-10-06 21:04:49 -0300

    refactor: move signup to auth module

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { compare } from 'bcryptjs';
+import { compare, hash } from 'bcryptjs';
 
 import { UsersRepository } from '../../shared/database/repositories/users.repositories';
 import { AuthenticateDto } from './dto/authenticate.dto';
+import { SignUpDto } from './dto/signup.dto';
 
 @Injectable()
 export class AuthService {
@@ -32,5 +33,50 @@ export class AuthService {
     });
 
     return { accessToken };
+  }
+
+  async signup(signUpDto: SignUpDto) {
+    const { name, email, password } = signUpDto;
+
+    const emailExists = await this.usersRepo.findUnique({
+      where: { email },
+      select: { id: true },
+    });
+
+    if (emailExists) {
+      throw new ConflictException('This email is already in use');
+    }
+
+    const hashedPassword = await hash(password, 12);
+
+    const user = await this.usersRepo.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        categories: {
+          createMany: {
+            data: [
+              // Income
+              { "name": "Salário", "icon": "travel", "type": "INCOME" },
+              { "name": "Freelance", "icon": "freelance", "type": "INCOME" },
+              { "name": "Outro", "icon": "other", "type": "INCOME" },
+              // Expense
+              { "name": "Casa", "icon": "home", "type": "EXPENSE" },
+              { "name": "Alimentação", "icon": "food", "type": "EXPENSE" },
+              { "name": "Educação", "icon": "education", "type": "EXPENSE" },
+              { "name": "Lazer", "icon": "fun", "type": "EXPENSE" },
+              { "name": "Mercado", "icon": "grocery", "type": "EXPENSE" },
+              { "name": "Roupas", "icon": "clothes", "type": "EXPENSE" },
+              { "name": "Transporte", "icon": "transport", "type": "EXPENSE" },
+              { "name": "Viagem", "icon": "travel", "type": "EXPENSE" },
+              { "name": "Outro", "icon": "other", "type": "EXPENSE" }
+            ]
+          }
+        }
+      }
+    });
+
+    return user;
   }
 }
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,18 +1,10 @@
 import {
-  Body,
-  Controller,
-  Post
+  Controller
 } from '@nestjs/common';
 
-import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
 }
