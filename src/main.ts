160b63b4d9ae497b9093cb1a8a1f73d15c4e0009
Ana Helena <anahelenarp@hotmail.com>
2025-10-10 10:04:26 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*', //TO-DO: change to specific domain in production
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
